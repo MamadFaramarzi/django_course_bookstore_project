c88ca11eab31f8f57b8885f732ec92fb392144d7
Mamadfaramarzi <mohammadfaramarzi.f78@gmail.com>
2023-10-26 13:59:53 +0330

    add pass test mixin to view

--- books/views.py
+++ books/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.urls import reverse, reverse_lazy
 from django.views import generic
 from django.shortcuts import get_object_or_404
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin , UserPassesTestMixin
 from django.contrib.auth.decorators import login_required
 
 from .models import Book, Comment
@@ -45,13 +45,22 @@ class BookCreateView(LoginRequiredMixin, generic.CreateView):
     template_name = "books/book_create.html"
 
 
-class BookUpdateView(LoginRequiredMixin, generic.UpdateView):
+class BookUpdateView(LoginRequiredMixin, UserPassesTestMixin, generic.UpdateView):
     model = Book
     fields = ['title', 'author', 'translator', 'description', 'price', 'cover']
     template_name = 'books/book_update.html'
 
+    def test_func(self):
+        obj = self.get_object()
+        return obj.user == self.request.user
 
-class BookDeleteView(LoginRequiredMixin, generic.DeleteView):
+
+class BookDeleteView(LoginRequiredMixin, UserPassesTestMixin, generic.DeleteView):
     model = Book
     template_name = "books/book_delete.html"
     success_url = reverse_lazy("book_list")
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.user == self.request.user
+
